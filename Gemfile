c61c0e19988eef753bd258d4cc884c763de4228f
Matt Buck <mattbuck@massrelevance.com>
2012-08-11 06:25:57 -0500

    Initialize middleman project.

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source :rubygems
+
+gem "middleman", "~>3.0.0"
+gem 'bootstrap-sass', '~> 2.0.4.0'
--- config.rb
+++ config.rb
@@ -1,26 +1,77 @@
-require 'bootstrap-sass'
-# Require any additional compass plugins here.
+### 
+# Compass
+###
 
+# Susy grids in Compass
+# First: gem install compass-susy-plugin
+# require 'susy'
 
-# Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+###
+# Page options, layouts, aliases and proxies
+###
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+# Per-page layout changes:
+# 
+# With no layout
+# page "/path/to/file.html", :layout => false
+# 
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+# 
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
 
+###
+# Helpers
+###
 
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+  
+  # Minify Javascript on build
+  # activate :minify_javascript
+  
+  # Enable cache buster
+  # activate :cache_buster
+  
+  # Use relative URLs
+  # activate :relative_assets
+  
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+  
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
